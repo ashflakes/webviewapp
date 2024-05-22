23ddb1987a140932dee2b52d3ef36205a370b11f
George Cristi <ashflakes@gmail.com>
2024-05-22 10:07:38 +0300

    onesignal package fix

--- App.js
+++ App.js
@@ -2,19 +2,16 @@ import { StyleSheet,} from 'react-native';
 import { WebView } from 'react-native-webview';
 import { LogLevel, OneSignal } from 'react-native-onesignal';
 
-
-
-
 export default function App() {
   OneSignal.Debug.setLogLevel(LogLevel.Verbose);
   OneSignal.initialize('ce015518-7f7e-4e34-bb69-15f84dd6dcb4');
   OneSignal.Notifications.requestPermission(true);
-  
+
   return (
     <WebView 
       style={styles.webView}
       source={{ uri: 'https://www.norocosulcastigator.com/' }}
-      />
+    />
   );
 }
 
